2bba02dc4671e26d66748bace2f934b495317dc0
hg <bhg5460@gmail.com>
2024-07-22 23:49:41 +0900

    inprog: redis 세션 클러스터링
    Signed-off-by: hg <bhg5460@gmail.com>

--- src/main/java/com/example/springsecurity_authority/SpringsecurityAuthorityApplication.java
+++ src/main/java/com/example/springsecurity_authority/SpringsecurityAuthorityApplication.java
@@ -2,8 +2,10 @@ package com.example.springsecurity_authority;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
+// @EnableRedisHttpSession // spring.session.store-type 지정 시 생략 가능
 public class SpringsecurityAuthorityApplication {
 
 	public static void main(String[] args) {
